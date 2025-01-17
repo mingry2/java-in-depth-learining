4c2f3235d5a0de5d92855e1346d703f13ec84f40
minkyung kim <minry20415@naver.com>
2025-01-17 15:29:44 +0900

    ✨: 애노테이션 서블릿
    - 동적 바인딩
    - 성능 최적화
    - 중복 경로 제거

---
+++ src/was/v8/SearchControllerV8.java
@@ -0,0 +1,18 @@
+package was.v8;
+
+import was.httpserver.HttpRequest;
+import was.httpserver.HttpResponse;
+import was.httpserver.servlet.annotation.Mapping;
+
+public class SearchControllerV8 {
+
+    @Mapping("/search")
+    public void search(HttpRequest request, HttpResponse response) {
+        String query = request.getParameter("q");
+        response.writerBody("<h1>search</h1>");
+        response.writerBody("<ul>");
+        response.writerBody("<li>query: " + query + "</li>");
+        response.writerBody("</ul>");
+    }
+
+}
